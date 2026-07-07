57eb14f5344bccb92cba566ce21850cf4f7c1009
TheRealStingos <justmo10@gmail.com>
2026-07-07 16:26:17 -0400

    Add publishJSON function

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+import amqp from "amqplib";
+import { ExchangePerilDirect } from "../internal/routing/routing.js";
+import { PauseKey } from "../internal/routing/routing.js";
+import { type PlayingState } from "../internal/gamelogic/gamestate.js";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  conn.createConfirmChannel();
+  console.log("Connected to RabbitMQ");
+  process.on("SIGINT", () => {
+    console.log("Closing RabbitMQ connection...");
+    conn.close();
+    process.exit(0);
+  })
 }
 
 main().catch((err) => {
