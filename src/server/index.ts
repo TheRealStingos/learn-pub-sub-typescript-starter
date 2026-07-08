b72ff15829001440da43eb5ffe2a13c2c9304741
TheRealStingos <justmo10@gmail.com>
2026-07-08 09:28:06 -0400

    add pause exchange routing

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,15 @@ import amqp from "amqplib";
 import { ExchangePerilDirect } from "../internal/routing/routing.js";
 import { PauseKey } from "../internal/routing/routing.js";
 import { type PlayingState } from "../internal/gamelogic/gamestate.js";
-
+import { publishJSON } from "../internal/pubsub/publish.js";
 async function main() {
   console.log("Starting Peril server...");
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
-  conn.createConfirmChannel();
+  const ch = await conn.createConfirmChannel();
   console.log("Connected to RabbitMQ");
+  await publishJSON(ch, ExchangePerilDirect, PauseKey, { isPaused: true })
+
   process.on("SIGINT", () => {
     console.log("Closing RabbitMQ connection...");
     conn.close();
