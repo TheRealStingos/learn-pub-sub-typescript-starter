ca93cd0a538b9fac7334e47784b77f425d016b28
TheRealStingos <justmo10@gmail.com>
2026-07-08 13:57:12 -0400

    add client function

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,15 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+import { ExchangePerilDirect } from "../internal/routing/routing.js";
+import { PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  const username = await clientWelcome();
+  await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient)
 }
 
 main().catch((err) => {
