6882be52f015f0c4c7e24eacc5eb97cb15dc3b77
TheRealStingos <justmo10@gmail.com>
2026-07-08 16:15:41 -0400

    add client REPL

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,15 +1,51 @@
 import amqp from "amqplib";
-import { clientWelcome, printServerHelp } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printClientHelp } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect } from "../internal/routing/routing.js";
 import { PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   console.log("Starting Peril client...");
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   const username = await clientWelcome();
-  await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient)
+  await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient);
+  const gameState = new GameState(username)
+
+  while (true) {
+    const input = await getInput()
+    if (input.length === 0) {
+      continue
+    }
+    if (input[0] === "spawn") {
+      commandSpawn(gameState, input)
+      continue
+    }
+    if (input[0] === "move") {
+      commandMove(gameState, input);
+      continue
+    }
+    if (input[0] === "status") {
+      commandStatus(gameState);
+      continue
+    }
+    if (input[0] === "help") {
+      printClientHelp();
+      continue
+    }
+    if (input[0] === "spam") {
+      console.log("Spamming is not allowed")
+      continue
+    }
+    else {
+      console.log("unknown command")
+      continue;
+    }
+
+  }
 }
 
 main().catch((err) => {
