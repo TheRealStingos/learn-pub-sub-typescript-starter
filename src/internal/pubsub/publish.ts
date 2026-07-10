bb66fcff7fe514f76357b1be3ffdf387ab5a3dc5
Stingos <Justmo10@gmail.com>
2026-07-10 15:44:23 -0400

    add msgpack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import { type ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export function publishJSON<T>(
     ch: ConfirmChannel,
@@ -9,11 +10,30 @@ export function publishJSON<T>(
     const serialized = Buffer.from(JSON.stringify(value));
     return new Promise((resolve, reject) => {
         ch.publish(exchange, routingKey, serialized, { contentType: "application/json" }, (err, result) => {
-            if (err) {
+            if (err !== null) {
                 reject(err)
             } else {
                 resolve()
             }
         });
+    })
+}
+
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+    const encoded = encode(value)
+    const serialized = Buffer.from(encoded)
+    return new Promise((resolve, reject) => {
+        ch.publish(exchange, routingKey, serialized, { contentType: "application/x-msgpack"}, (err, result) => {
+            if (err !== null) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
     })
 }
